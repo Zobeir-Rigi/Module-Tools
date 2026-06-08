16e732d0cba85582013ed744aac5920f392dba5f
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-08 22:35:00 +0100

    triple function instead of double

--- sprint-five-prep/first.py
+++ sprint-five-prep/first.py
@@ -7,5 +7,15 @@
 def double(value):
     return value * 2 
 
-
 print(double("22"))
+
+# ---------------
+def double(number):
+    return number * 3
+
+print(double(10))
+
+# ✍️exercise
+# Read the above code and write down what the bug is. How would you fix it?
+# it will return 30, but the function name is double, so we have to change the function name to triple or change the return statement to number * 2. 
+
