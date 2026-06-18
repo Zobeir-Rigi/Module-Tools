b6e7740d31fa45db79913ff78ce8b945ee58996d
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-18 18:03:30 +0100

    n(numerically) r(reverse) -k3,3 → sort by column 3 only, not the whole line, and head -3 → show the first 3 lines of the sorted output

--- shell-pipelines/sort-uniq-head-tail/script-03.sh
+++ shell-pipelines/sort-uniq-head-tail/script-03.sh
@@ -8,4 +8,7 @@ set -euo pipefail
 # Basia London 22 9 6
 # Piotr Glasgow 15 2 25 11 8
 # Chandra Birmingham 12 6
-sort 
+
+sort -k3,3nr scores-table.txt | head -3
+  # sort by column 3 numerically (highest first) and show top 3 lines.
+#    n(numerically) r(reverse) -k3,3 → sort by column 3 only, not the whole line, and head -3 → show the first 3 lines of the sorted output
