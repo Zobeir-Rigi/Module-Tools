e487d4957c5d98e3306df39aaf31b7ad55a7ac7b
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-18 17:49:05 +0100

    using -c to count matching lines

--- shell-pipelines/ls-grep/script-04.sh
+++ shell-pipelines/ls-grep/script-04.sh
@@ -4,4 +4,11 @@ set -euo pipefail
 
 # TODO: Write a command to count the number of files in the sample-files directory whose name starts with an upper case letter and doesn't contain any other upper case letters.
 # Your output should be the number 7.
-ls sample-files | grep -E '^[A-Z][^A-Z]*$' | wc -l
+ ls sample-files | grep -c -E '^[A-Z][^A-Z]*$'
+# -c → tells grep to count matching lines instead of printing them
+
+
+# -E → enables extended regex (more flexible syntax)
+# ^[A-Z] → starts with uppercase
+# [^A-Z]* → no other uppercase letters allowed
+# $ → match the whole filename
