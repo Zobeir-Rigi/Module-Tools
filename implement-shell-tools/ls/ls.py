b381274364ab8ec94fd26bd3a5da48e4da75ac9b
Zobeir-Rigi <rigi.zobir@yahoo.com>
2026-06-19 02:07:04 +0100

    Handle -1 flag explicitly and sort file listing

--- implement-shell-tools/ls/ls.py
+++ implement-shell-tools/ls/ls.py
@@ -4,20 +4,20 @@ import os
 args = sys.argv[1:]
 
 show_all = False
+one_per_line = False
 path = "."
 
 for arg in args:
-
     if arg == "-a":
         show_all = True
-
-    elif arg != "-1":
+    elif arg == "-1":
+        one_per_line = True
+    else:
         path = arg
 
-files = os.listdir(path)
+files = sorted(os.listdir(path))
 
 for file in files:
-
     if not show_all and file.startswith("."):
         continue
 
